b202fec8d3a5244641c01f278a77d5749c886b40
William Edwards <spaceiskool1998@gmail.com>
2023-05-01 17:18:25 -0400

    I have the submit button working and i am able to post the data to the query and it is also published so you can see it on the pasts page

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,10 +1,15 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+const httpLink = createHttpLink({
+  uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clh46qagr5b3901umbqn54cux/master',
+});
 
 const client = new ApolloClient({
-  uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clh46qagr5b3901umbqn54cux/master',
+  link: httpLink,
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODI5MTUzNjcsImF1ZCI6WyJodHRwczovL2FwaS11cy1lYXN0LTEtc2hhcmVkLXVzZWExLTAyLmh5Z3JhcGguY29tL3YyL2NsaDQ2cWFncjViMzkwMXVtYnFuNTRjdXgvbWFzdGVyIiwibWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6IjhmM2QwZWM5LWY1MWQtNDQ1ZC04ZTMwLWZjNzU4MjJjOGFjYyIsImp0aSI6ImNsaDRjY3dtZTViMzUwMXVtMWgyeTV5aTYifQ.wM6Bw_WGfMEUBfMRJkAvLhW2SAkAy4SNux5s8VcuPUS9dnEfCvLF9jTzc7zTGRLpVHfleqFrWEYvFTvX0XA7J4x1_F9eIMlVF9sbBHmnIJ16WWy_0761utp5lAx6U-7c81gjhKmMvIQd8Y028Bnlb7wErSUL1lY2DkUaPjr_yHUHjv-h-h0JpsGWtLIq9KU_VAUGi54IqiYtn-xXMHwQ_738pDU-QnOSuc-CXnlchTv8CR0ozpDhAAsvgBHUo1HajgY-ZpOmW0lh4HYpMGMPP4fu8eUz5unSP9zAGyeR2azp9UX-D8h2pFOXTCQiKhF7HxmkyCEEDWRPHDtWAlAH9vgvi1G8_BQeL7cYMRe0WsKZzaLNQTHqz8ogmJk4Zv9eYByfRtSkQnEhIyzgmnMzHmEm6cQA8jj7-OgDUdpc6vIhy7GHDatzg5xEgib2pQeYdVVJJpFzc7B6ucEISsc27e7N1BPQKaW3RDdauIUcurGq6T6FULxcEgjysH7RauP6Mj_geouH5Yq15ILkmAnU14FcGVJAPs3VXw3Xz5Zqidr9ihPk40sZxQayqX-Z6SvXoAEITr3Lv-cQFqWeqjYtHmo9vThETDnGQX4lVmv7HtKK29dQldlxRz8PUpLZq-BNSMZgXRswSA8Ki35hN1kZqol4IxmRS8UHYj1Slg6PTSg`,
+  },
 });
 
 export default client;
-
--- src/pages/entries.js
+++ src/pages/entries.js
@@ -12,9 +12,30 @@ import LogoutIcon from '@mui/icons-material/Logout';
 
 
 const CREATE_JOURNAL_ENTRY = gql`
-  mutation createJournal($username: String!,$accomplishment: String!, $gratitude: String!, $learning: String!, $lookingForward: String!, $struggle: String!, $entryDate: Date!) {
-    createJournal(data: {username: $username,accomplishment: $accomplishment, gratitude: $gratitude, learning: $learning, lookingForward: $lookingForward, struggle: $struggle, entryDate: $entryDate}) {
-      id
+  mutation createJournal(
+    $username: String!,
+    $accomplishment: String!,
+    $gratitude: String!,
+    $learning: String!,
+    $lookingForward: String!,
+    $struggle: String!,
+    $entryDate: Date!
+    
+  ) {
+    createJournal(
+      data: {
+        username: $username,
+        accomplishment: $accomplishment,
+        gratitude: $gratitude,
+        learning: $learning,
+        lookingForward: $lookingForward,
+        struggle: $struggle,
+        entryDate: $entryDate
+        
+        
+      }
+    ) {
+      
       username
       accomplishment
       gratitude
@@ -22,20 +43,26 @@ const CREATE_JOURNAL_ENTRY = gql`
       lookingForward
       struggle
       entryDate
+      id
+      stage
+     
     }
   }
 `;
 
 const PUBLISH_JOURNAL_ENTRY = gql`
   mutation publishJournal($id: ID!) {
-    updateJournal(
-      where: { id: $id },
-      
+    publishJournal(
+      where: { id: $id }
+      to: PUBLISHED
+
     ) {
       id
+      
     }
   }
 `;
+
 
 
 
@@ -52,7 +79,6 @@ function JournalEntryForm({ restId }) {
   const [createJournalEntry, { loading: createLoading, error: createError, data }] = useMutation(CREATE_JOURNAL_ENTRY, { client });
   const [publishJournalEntry, { loading: publishLoading, error: publishError }] = useMutation(PUBLISH_JOURNAL_ENTRY, { client });
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     const isoDate = new Date(entryDate).toISOString();
@@ -75,31 +101,46 @@ function JournalEntryForm({ restId }) {
       });
       console.log('createJournalEntry data:', data);
       const entryId = data.createJournal.id;
+      const published = data.createJournal.stage;
       console.log(entryId);
-      await publishJournalEntry({ variables: { id: entryId } });
-      console.log('Journal entry published successfully');
-      navigate(`/journal/${entryId}`);
-    } catch (error) {
-      console.error('Error occurred while submitting journal entry:', error);
-      // Handle error here
-    }
-  };
+      console.log(published);
+      
+      await publishJournalEntry({ variables: { id: entryId }, data: { stage: published } });
 
- 
+      console.log(published);
+      
+      console.log('Journal entry published successfully');
+    } catch (error) {
+      console.error('Error occurred while publishing journal entry:', error);
+    }
+  
+      
+      // Handle error here
+    
+  };
+  React.useEffect(() => {
+    if (createError) {
+      console.error('Error occurred while creating journal entry:', createError);
+    }
+    if (publishError) {
+      console.error('Error occurred while publishing journal entry:', publishError);
+    }
+  }, [createError, publishError]);
+
   if (createLoading || publishLoading) {
     return <p>Loading...</p>;
   }
 
-  if (createError ) {
+  if (createError) {
     return <p>Error : createError {createError?.message}</p>;
-}
-if (publishError ) {
-  return <p>Error : publishError   {publishError?.message}</p>;
-}
+  }
+  if (publishError) {
+    return <p>Error : publishError   {publishError?.message}</p>;
+  }
 
 
-  
-  
+
+
   const goToHome = () => {
     navigate("/basepage");
   };
@@ -118,7 +159,7 @@ if (publishError ) {
 
   return (
     <>
-    <center>
+      <center>
         <div>
           <h1>Journal Entry {restId} </h1>
         </div>
@@ -130,47 +171,47 @@ if (publishError ) {
           <IconButton size="small" variant="contained" color="primary" onClick={(e) => goToLogin()}><LogoutIcon /></IconButton>
         </Stack>
         <Card sx={{ width: 650 }} style={{ backgroundColor: '#83C5BE' }}><CardContent>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>      
-    <form onSubmit={handleSubmit}>
-      <label>
-      What is one thing you accomplished today?<br/><br/>
-        <textarea name="accomplishment" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={accomplishment} onChange={(e) => setAccomplishment(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-      What is one thing you are grateful for today?<br/><br/>
-      <textarea name="gratitude" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={gratitude} onChange={(e) => setGratitude(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-      What is one thing you learned today?<br/><br/>
-      <textarea name="learning" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={learning} onChange={(e) => setLearning(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-      What is one thing you are looking forward to tomorrow?<br/><br/>
-      <textarea name="lookingForward" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={lookingForward} onChange={(e) => setLookingForward(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-      What is one thing you struggled with today?<br/><br/>
-      <textarea name="struggle" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={struggle} onChange={(e) => setStruggle(e.target.value)} />
-      </label>
-      <br/>
-      <label>
-       What is your Username: <br/><br/>
-       <textarea name="username" placeholder="Write your response here" cols="50" rows="2" style={{ resize: "none" }} value={username} onChange={(e) => setUsername(e.target.value)} />
-       </label>
-      <br/>
-      <label>
-        Entry Date:<br/><br/>
-        <input type="date" value={entryDate} onChange={(e) => setEntryDate(e.target.value)} />
-        </label>
-      <br/>
-      <button type="submit">Submit</button>
-    </form>
-    </div>
-    </CardContent></Card><br/>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <form onSubmit={handleSubmit}>
+              <label>
+                What is one thing you accomplished today?<br /><br />
+                <textarea name="accomplishment" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={accomplishment} onChange={(e) => setAccomplishment(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                What is one thing you are grateful for today?<br /><br />
+                <textarea name="gratitude" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={gratitude} onChange={(e) => setGratitude(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                What is one thing you learned today?<br /><br />
+                <textarea name="learning" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={learning} onChange={(e) => setLearning(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                What is one thing you are looking forward to tomorrow?<br /><br />
+                <textarea name="lookingForward" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={lookingForward} onChange={(e) => setLookingForward(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                What is one thing you struggled with today?<br /><br />
+                <textarea name="struggle" placeholder="Write your response here" cols="50" rows="3" style={{ resize: "none" }} value={struggle} onChange={(e) => setStruggle(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                What is your Username: <br /><br />
+                <textarea name="username" placeholder="Write your response here" cols="50" rows="2" style={{ resize: "none" }} value={username} onChange={(e) => setUsername(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                Entry Date:<br /><br />
+                <input type="date" value={entryDate} onChange={(e) => setEntryDate(e.target.value)} />
+              </label>
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </CardContent></Card><br />
       </center>
     </>
   );
@@ -180,7 +221,7 @@ function JournalEntries({ restId }) {
   return (
     <ApolloProvider client={client}>
       <div>
-        
+
         <JournalEntryForm restId={restId} />
       </div>
     </ApolloProvider>
