5de02ada14edbafb873ca9d6e0f15291bfe5d01e
William Edwards <spaceiskool1998@gmail.com>
2023-05-03 19:35:07 -0400

    Hard coded a username and password

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,37 +1,62 @@
 import { TextField, Card, CardContent } from '@mui/material';
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-
-const Login = ({restId, setRestId}) => {
-    const navigate = useNavigate()
-    const goToHome = () => {
-        
-        navigate("/basepage")
-      }
-    
-      return (
-        <center>
-          <center><h1>Welcome {restId}</h1></center>
-          
-          <Card sx={{ width: 275 }}>
-            <CardContent>
-              <form>
-                <TextField id="username" label="Username:" variant="standard" />
-                <br />
-                <TextField id="password" label="Password:" type="password" variant="standard" />
-                <br />
-                <Button size="small" onClick={() => goToHome()}>Login</Button>
-                <br />
-                <Button variant="text" aria-label="Create Account">Create Account</Button>
-              </form>
-            </CardContent>
-          </Card>
-        </center>
-      )
+    // Check if the entered username and password match the hardcoded values
+    if (username === '' && password === '') {
+      navigate('/basepage');
+    } else {
+      alert('Invalid username or password');
     }
-    
-    export default Login
+  };
+
+  return (
+    <center>
+      <center>
+        <h1>Login</h1>
+      </center>
+
+      <Card sx={{ width: 275 }}>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <TextField
+              id="username"
+              label="Username:"
+              variant="standard"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <TextField
+              id="password"
+              label="Password:"
+              type="password"
+              variant="standard"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <Button type="submit" size="small">
+              Login
+            </Button>
+            <br />
+            <Button variant="text" aria-label="Create Account">
+              Create Account
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </center>
+  );
+};
+
+export default Login;
